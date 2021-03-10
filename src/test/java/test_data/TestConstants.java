3151941671e23a895fb851524e82d6558f813b8f
Kate White <Shmatok2020>
2021-03-10 22:45:54 +0300

    Fixed all

--- src/test/java/test_data/TestConstants.java
+++ src/test/java/test_data/TestConstants.java
@@ -8,8 +8,8 @@ public interface TestConstants {
     String FIELD_ZIPCODE = "22038";
     String FIELD_MOB_PHONE = "+375259424336";
     String EMPTY_EMAIL_ADDRESS = "";
-    String WRONG_EMAIL_ADDRESS_MESSAGE = "ivanov";
-    String EXPECTED_RESULT_WRONG_EMAIL_ADDRESS = "Invalid email address.";
+    String WRONG_EMAIL_ADDRESS = "ivanov";
+    String WRONG_EMAIL_ADDRESS_MESSAGE = "Invalid email address.";
     String FIELD_YOUR_FIRSTNAME = "Anton";
     String FIELD_YOUR_FIRSTNAME_COMPANY = "Anton";
     String EMPTY_FIELD_FIRST_NAME = "";
--- src/test/java/tests/CreateAnAccountTests.java
+++ src/test/java/tests/CreateAnAccountTests.java
@@ -3,13 +3,13 @@ package tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import static constants.CommonConstants.SIGN_IN;
+import static constants.CommonConstants.SIGN_IN_URL;
 
 public class CreateAnAccountTests extends BaseTest {
 
     @Test(groups = "Katia. Empty email address verification.")
     public void firstnameFieldIsEmptyTest() {
-        createAnAccountPage.openPage(SIGN_IN);
+        createAnAccountPage.openPage(SIGN_IN_URL);
         createAnAccountPage.fillInEmailAndClickCreateAccountBtn(EMAIL_ADDRESS);
         createAnAccountPage.inputTextInFormFirstNameAndClickRegistr(EMPTY_FIELD_FIRST_NAME);
         Assert.assertEquals(createAnAccountPage.getEmptyFirstnameText(), EXPECTED_RESULT_EMPTY_FIELD_FIRSTNAME);
@@ -17,7 +17,7 @@ public class CreateAnAccountTests extends BaseTest {
 
     @Test(groups = "Katia. Input short-zipcode verification.")
     public void inputLongPasswordTest() {
-        createAnAccountPage.openPage(SIGN_IN);
+        createAnAccountPage.openPage(SIGN_IN_URL);
         createAnAccountPage.fillInEmailAndClickCreateAccountBtn(EMAIL_ADDRESS);
         createAnAccountPage.inputShortPswrdInField(SHORT_ZIPCODE);
         Assert.assertEquals(createAnAccountPage.getShortPswrdInField(), EXPECTED_RESULT_NOT_LONG_PASSWORD);
@@ -25,7 +25,7 @@ public class CreateAnAccountTests extends BaseTest {
 
     @Test(groups = "Katia. The required form fields are filled. Registration is complete.")
     public void allRequiredFieldsAreFilled() {
-        createAnAccountPage.openPage(SIGN_IN);
+        createAnAccountPage.openPage(SIGN_IN_URL);
         createAnAccountPage.fillInEmailAndClickCreateAccountBtn(EMAIL_ADDRESS);
         createAnAccountPage.inptTextInRequiredFieldsFormAndReg(FIELD_YOUR_FIRSTNAME,
                 FIELD_YOUR_LASTNAME,
