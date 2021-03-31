c699c19e969365242c6cf3fb11708fda3d9553ed
Kate White <Shmatok2020>
2021-04-01 01:23:43 +0300

    created class Customer

---
+++ src/test/java/tests/Customer.java
@@ -0,0 +1,26 @@
+package tests;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Customer extends BaseTest {
+    private String emailAddress;
+    private String yourFirstName;
+    private String yourLastName;
+    private String password;
+    private String firstNameCompany;
+    private String lastNameCompany;
+    private String addressCompany;
+    private String city;
+    private String state;
+    private String zipCode;
+    private String country;
+    private String phoneNumber;
+    private String assignCompanyName;
+}
